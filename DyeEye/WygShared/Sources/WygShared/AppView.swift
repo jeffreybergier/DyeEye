efd1a9c033b256d61367472cbdac65916024d957
jeffreybergier <jeffburg@jeffburg.com>
2022-10-05 19:28:59 +0900

    got a basic stepper working

--- DyeEye/WygShared/Sources/WygShared/AppView.swift
+++ DyeEye/WygShared/Sources/WygShared/AppView.swift
@@ -25,30 +25,14 @@
 //
 
 import SwiftUI
-import ActivityKit
-import WygShared
 
-@main
-internal struct DyeEyeApp: App {
+public struct AppView: View {
     
-    @State internal var activity: Activity<Attributes>?
+    @Storage private var value
     
-    internal var body: some Scene {
-        WindowGroup {
-            VStack() {
-                AppView()
-                AppView()
-                Spacer()
-            }
-            .onAppear() {
-                let initialContentState = Attributes.ContentState(value: 5)
-                let activityAttributes = Attributes(name: "Goodbye World")
-                do {
-                    self.activity = try Activity.request(attributes: activityAttributes, contentState: initialContentState)
-                } catch (let error) {
-                    print("Error requesting pizza delivery Live Activity \(error.localizedDescription).")
-                }
-            }
-        }
+    public init() {}
+    
+    public var body: some View {
+        Stepper(String(self.value.count), value: self.$value.count)
     }
 }
